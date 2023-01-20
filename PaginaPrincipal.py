16ad04628bb5d262084cb260ecd0c62a9b144de0
conradozb <35931560+conradozb@users.noreply.github.com>
2023-01-20 13:22:42 -0600

    Update PaginaPrincipal.py

--- PaginaPrincipal.py
+++ PaginaPrincipal.py
@@ -7,10 +7,7 @@ Control de Cursos. Manejo de profesores.
 """
 import streamlit as st
 #PaginaPrincipal.py
-st.title('Pagina principal del prototipo de gestion de cursos')
-st.header("Aplicacion web creada con Python 3.9.15, Streamlit 1.11 y \
-          ag-grid 18.0.1, bajo entorno de Windows 10 e\
-              IDE Spyder 5.3.3 de Anaconda Navigator 2.3.2")             
+st.title('Pagina principal del prototipo de gestion de cursos')           
 st.subheader("Utiliza archivos 'csv' para almacenamiento de datos")
 
 
